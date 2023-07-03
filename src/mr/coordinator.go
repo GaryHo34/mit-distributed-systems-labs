78314900bd9366e5a17a2b472d5ce1729ec45174
Gary Ho <garyho0916@gmail.com>
2023-07-03 10:11:03 +0800

    Define the coordinator struct

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -6,11 +6,21 @@ import (
 	"net/http"
 	"net/rpc"
 	"os"
+	"sync"
+	"time"
 )
+
+type WorkDetail struct {
+	IsSuccess bool
+	StartTime time.Time
+}
 
 type Coordinator struct {
 	// Your definitions here.
-
+	mu         sync.Mutex      // an internal lock
+	taskqueue  []Work          // a taskqueue
+	reduceWork map[int]bool    // a record if reduceWork all success
+	mapWork    map[string]bool // a record if  mapwork all success
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -20,6 +30,44 @@ type Coordinator struct {
 // the RPC argument and reply types are defined in rpc.go.
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
+	return nil
+}
+
+func (c *Coordinator) GetWork(reply *WorkReply) error {
+	c.mu.Lock()
+	if len(c.taskqueue) == 0 {
+		reply.HasWork = false
+		return nil
+	}
+	reply.Work = c.taskqueue[0]
+	c.taskqueue = c.taskqueue[1:]
+	c.mu.Unlock()
+	return nil
+}
+
+func (c *Coordinator) ReplyFinish(args *WorkArgs) error {
+	c.mu.Lock()
+	switch args.WorkType {
+	case MAP:
+		if args.IsSuccess {
+			c.mapWork[args.MapWork.Filename] = true
+		} else {
+			c.taskqueue = append(c.taskqueue, Work{
+				WorkType: MAP,
+				MapWork:  args.MapWork,
+			})
+		}
+	case REDUCE:
+		if args.IsSuccess {
+			c.reduceWork[args.ReduceWork.ReduceIndex] = true
+		} else {
+			c.taskqueue = append(c.taskqueue, Work{
+				WorkType:   REDUCE,
+				ReduceWork: args.ReduceWork,
+			})
+		}
+	}
+	c.mu.Unlock()
 	return nil
 }
 
@@ -40,9 +88,16 @@ func (c *Coordinator) server() {
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
 func (c *Coordinator) Done() bool {
-	ret := false
+	ret := true
 
 	// Your code here.
+	for _, v := range c.mapWork {
+		ret = ret && v
+	}
+
+	for _, v := range c.reduceWork {
+		ret = ret && v
+	}
 
 	return ret
 }
@@ -51,10 +106,29 @@ func (c *Coordinator) Done() bool {
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		taskqueue:  make([]Work, 0),
+		reduceWork: make(map[int]bool),
+		mapWork:    make(map[string]bool),
+	}
 
-	// Your code here.
+	for idx, file := range files {
+		c.mapWork[file] = false
+		c.taskqueue = append(c.taskqueue, Work{
+			WorkType: MAP,
+			MapWork: MapWork{
+				Filename:  file,
+				FileIndex: idx,
+				NReduce:   nReduce,
+			},
+		})
+	}
+
+	for i := 0; i < nReduce; i++ {
+		c.reduceWork[i] = false
+	}
 
 	c.server()
+
 	return &c
 }
--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -26,6 +26,43 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+// the type of the work, MAP or REDUCE
+type WorkType int
+
+const (
+	MAP WorkType = iota
+	REDUCE
+)
+
+// The information needed for Map type work
+type MapWork struct {
+	Filename  string
+	FileIndex int // This is a convention for mr-X index
+	NReduce   int // how many reduce files
+}
+
+type ReduceWork struct {
+	ReduceIndex int // to know witch mr-X-Y file it needs to work on
+}
+
+type Work struct {
+	WorkType   WorkType
+	MapWork    MapWork
+	ReduceWork ReduceWork
+}
+
+type WorkReply struct {
+	HasWork bool
+	Work    Work
+}
+
+type WorkArgs struct {
+	WorkType   WorkType
+	MapWork    MapWork
+	ReduceWork ReduceWork
+	IsSuccess  bool
+}
+
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
 // Can't use the current directory since
